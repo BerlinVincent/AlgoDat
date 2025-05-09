158a51d657df1ab49c5b66339a9ee0178838f84f
Vincent <vincent.sternberg@gmail.com>
2025-05-09 10:28:38 +0200

    Live-Code aus Tutorium 1

--- Testspace/src/Main.java
+++ Testspace/src/Main.java
@@ -1,9 +1,59 @@
+abstract class Wirbeltiere {
+    double alter;
+    int anzahlWirbel;
+    String name;
+    int kalorien;
+
+    public int essen() {
+        System.out.println(kalorien);
+        this.kalorien++;
+        System.out.println(kalorien);
+        return 0;
+    }
+}
+
+abstract class Saeugetiere extends Wirbeltiere {
+    Saeugetiere(String name) {
+        this.name = name;
+    }
+}
+abstract class Reptilien extends Wirbeltiere {}
+
+class Okapi extends Saeugetiere {
+    public Okapi(String name) {
+        super(name);
+    }
+}
+
+class Kegelrobbe extends Saeugetiere {
+    String geraeusch;
+    int Umfang;
+
+    @Override
+    public int essen() {
+        System.out.println(Umfang);
+        this.Umfang++;
+        System.out.println(Umfang);
+        return super.essen();
+    }
+
+    public Kegelrobbe(String name, int umfang, String geraeusch) {
+        super(name);
+        this.Umfang = umfang;
+        this.geraeusch = geraeusch;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        Kegelrobbe Thorsten = new Kegelrobbe("Thorsten", 50, "Oempfh");
+        Okapi Thorsten2 = new Okapi("Thorsten");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Saeugetiere[] array = new Saeugetiere[10];
+        array[0] = Thorsten;
+        array[1] = Thorsten2;
+
+        Thorsten.essen();
+        Thorsten2.essen();
     }
 }
