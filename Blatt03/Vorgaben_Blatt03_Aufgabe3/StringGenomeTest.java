6b9a29193c7853f47df4f330cdfd2a36725bb8bd
Vincent <v.sternberg@campus.tu-berlin.de>
2025-05-20 17:00:15 +0200

    Lösung für Tutoriumsaufgabe
    Signed-off-by: Vincent <vincent.sternberg@gmail.com>

--- Blatt03/Vorgaben_Blatt03_Aufgabe3/StringGenomeTest.java
+++ Blatt03/Vorgaben_Blatt03_Aufgabe3/StringGenomeTest.java
@@ -1,4 +1,3 @@
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -6,27 +5,63 @@ public class StringGenomeTest {
 
 	@Test
 	public void testAddNucleotide() {
-		//TODO
+		// Versuchsobjekt und Vergleichswert
+		StringGenome genome = new  StringGenome();
+		String temp = genome.toString();
+
+		// Methode mit gültigem Wert aufrufen und erwartetes Ergebnis überprüfen
+		genome.addNucleotide('A');
+		assertEquals(temp + 'A', genome.toString());
+
+		// Methode mit ungültigem Wert aufrufen und erwartete Exception auffangen
+		try {
+			genome.addNucleotide('B');
+		} catch (RuntimeException e) {
+			assertNotEquals(temp + 'B', genome.toString());
 		}
+	}
 
 	@Test
 	public void testNucleotideAt() {
-	//TODO
+		// Versuchsobjekt
+		StringGenome genome = new  StringGenome("ACGT");
+
+		assertEquals('A', genome.nucleotideAt(0));
+		assertNotEquals('A', genome.nucleotideAt(1));
+
+		try {
+			genome.nucleotideAt(4);
+		} catch (RuntimeException e) {
+			assert true;
+		}
 	}
 
 	@Test
 	public void testLength() {
-	//TODO
+		// Versuchsobjekt
+		StringGenome genome = new  StringGenome("ACGT");
+
+		assertEquals(4, genome.length());
+		assertNotEquals(0, genome.length());
 	}
 
 	@Test
 	public void testToString() {
-	//TODO
+		// Versuchsobjekt
+		StringGenome genome = new  StringGenome("ACGT");
+
+		assertEquals("ACGT", genome.toString());
+		assertNotEquals("ACUT", genome.toString());
 	}
 
 	@Test
 	public void testEqualsObject() {
-	//TODO
-	}
+		// Versuchsobjekte
+		StringGenome genome1 = new  StringGenome("ACGT");
+		StringGenome genome2 = new  StringGenome(genome1);
+		StringGenome genome3 = new  StringGenome("ACUT");
 
+		assertTrue(genome1.equals(genome2));
+		assertFalse(genome1.equals(genome3));
+	}
 }
